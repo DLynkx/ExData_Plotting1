1c0e70c7ca0b5adcaf8686b0945b55f2af025a3d
Frank Dono <franklynkx@gmail.com>
2020-10-21 07:41:56 +0000

    revision1 on plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
 ## Read entire data
+library(lubridate)
 
 pow <- read.csv2("./exdata_data_household_power_consumption/household_power_consumption.txt")
 
@@ -13,6 +14,6 @@ globalactivepower <- as.numeric(subpow$Global_active_power)
 
 
 ## Plotting
-png("plot1.png")
+#png("plot1.png")
 hist(globalactivepower, xlab = "Global Active Power (kilowatts)", col = "red", main = "Global Active Power")
-dev.off()
+#dev.off()
